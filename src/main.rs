32875884d48baf2751596058a01bd5a357f1d71d
Mark Ellis <m@rkellis.com>
2019-12-02 22:39:56 +0000

    seems to be working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::{thread, time};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use rspotify::spotify::client::Spotify;
+use rspotify::spotify::util::get_token;
+use rspotify::spotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
 
 fn main() -> std::io::Result<()> {
-    // read this from clap
+    let mut oauth = SpotifyOAuth::default()
+        .scope("user-read-private playlist-modify-private")
+        .build();
+    let token_info = get_token(&mut oauth).unwrap();
+    let client_credential = SpotifyClientCredentials::default()
+        .token_info(token_info)
+        .build();
+    // Or set client_id and client_secret explictly
+    // let client_credential = SpotifyClientCredentials::default()
+    //     .client_id("this-is-my-client-id")
+    //     .client_secret("this-is-my-client-secret")
+    //     .build();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+
+    // create playlist
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+    println!("{:?}", now);
+    let user_id = "utrch63s7u3bha3i01phwkujg";
+    let playlist_name = format!("playlist {}", now);
+    let playlist = spotify.user_playlist_create(user_id, &playlist_name, false, None).unwrap();
+
     let mut file = File::open("/home/mark/Documents/music/good.m3u")?;
     let mut buffer = String::new();
 
@@ -28,13 +56,13 @@ fn main() -> std::io::Result<()> {
                 continue;
             }
         };
-        let album = match tag.album() {
-            Some(v) => v,
-            None    => {
-                println!("no album for {}", file);
-                continue;
-            }
-        };
+        // let album = match tag.album() {
+        //     Some(v) => v,
+        //     None    => {
+        //         println!("no album for {}", file);
+        //         continue;
+        //     }
+        // };
         let title = match tag.title() {
             Some(v) => v,
             None    => {
@@ -43,8 +71,29 @@ fn main() -> std::io::Result<()> {
             }
         };
 
-        println!("'{}' — '{}' — '{}'", artist, album, title);
+        let query = format!("artist:\"{}\" AND track:\"{}\"", artist, title);
+        let result = spotify.search_track(query.as_str(), 10, 0, None);
+        if let Ok(result) = result {
+            if let Some(track) = result.tracks.items.get(0) {
+                // println!("{}", query);
+                // println!("search result: {}", track.uri);
+                if let Some(track_id) = track.id.clone() {
+                    spotify.user_playlist_add_tracks(user_id, &playlist.id, &[track_id], None).unwrap();
+                }
+                else {
+                    println!("failed to add {} - {} to playlist", query, track.uri)
+                }
+            }
+        }
+        else {
+            println!("nothing found for {}", query);
+        };
+
+        //sleep so we don't get rate limited
+        thread::sleep(time::Duration::from_secs(2));
     }
 
     Ok(())
 }
+
+
